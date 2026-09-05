b39846f6a6db7d2ad99a378d302323c96f9b8315
tiga-kuga <maskedrider555.kabuto.w@gmail.com>
2026-09-06 00:22:56 +0900

    Add study app

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='network-study-v1';
+const ASSETS=['./','./index.html','./data.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
