0e8f42044ce656743ef72398cac2ad261fdd7a29
tiga-kuga <maskedrider555.kabuto.w@gmail.com>
2026-09-13 00:06:25 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='my-study-v42';
+const CACHE='my-study-v43';
 const CORE=['./','./index.html','./data.js','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install', event => {
